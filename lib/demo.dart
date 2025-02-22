29d0779db240a81fb4feae9b7d50bcbaa16ee438
Matteo Scanzano <ing.matteo.scanzano@gmail.com>
2025-02-22 17:39:36 +0000

    Add first 2 tests (ChatGPT), with home page with page selection buttons

--- lib/demo.dart
+++ lib/demo.dart
@@ -2,37 +2,9 @@
 
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
+class DemoPage extends StatefulWidget {
+  const DemoPage({Key? key}) : super(key: key);
 
   // This widget is the home page of your application. It is stateful, meaning
   // that it has a State object (defined below) that contains fields that affect
@@ -43,13 +15,13 @@ class MyHomePage extends StatefulWidget {
   // used by the build method of the State. Fields in a Widget subclass are
   // always marked "final".
 
-  final String title;
+  final String title = "Demo app";
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<DemoPage> createState() => _DemoPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _DemoPageState extends State<DemoPage> {
   int _counter = 0;
 
   void _incrementCounter() {
@@ -73,7 +45,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
+        // Here we take the value from the DemoPage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
--- lib/scouting1.dart
+++ lib/scouting1.dart
@@ -1,239 +1,42 @@
-// first tests with ChatGPT
+// first test (ChatGPT)
 
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Branco Chat',
-      home: ChatScreen(),
-      debugShowCheckedModeBanner: false,
-    );
-  }
-}
+class ChatPageTest1 extends StatelessWidget {
+  const ChatPageTest1({super.key});
 
-class ChatScreen extends StatefulWidget {
-  @override
-  _ChatScreenState createState() => _ChatScreenState();
-}
-
-class _ChatScreenState extends State<ChatScreen> {
-  final List<Message> messages = [
-    Message(
-      username: 'Alice',
-      text: 'Hello!',
-      isAdmin: false,
-      timestamp: DateTime.now().subtract(Duration(minutes: 5)),
-      status: MessageStatus.sent,
-    ),
-    Message(
-      username: 'Bob',
-      text: 'Hi there!',
-      isAdmin: true,
-      timestamp: DateTime.now().subtract(Duration(minutes: 4)),
-      status: MessageStatus.sent,
-    ),
-  ];
-
-  final TextEditingController _controller = TextEditingController();
-  final ScrollController _scrollController = ScrollController();
-  bool autoScroll = true;
-
-  void _sendMessage() {
-    if (_controller.text.trim().isEmpty) return;
-    setState(() {
-      messages.add(Message(
-        username: 'You',
-        text: _controller.text.trim(),
-        isAdmin: false,
-        timestamp: DateTime.now(),
-        status: MessageStatus.sending,
-      ));
-      _controller.clear();
-    });
-    _animateScroll();
-    // TODO: Integrate with PocketBase backend for sending message
-  }
-
-  void _animateScroll() {
-    if (autoScroll) {
-      _scrollController.animateTo(
-        _scrollController.position.maxScrollExtent,
-        duration: Duration(milliseconds: 300),
-        curve: Curves.easeOut,
-      );
-    }
-  }
-
-  Widget _buildMessageBubble(Message message) {
-    return ListTile(
-      leading: CircleAvatar(child: Text(message.username[0])),
-      title: Row(
-        children: [
-          Text(
-            message.username,
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: _getRandomColor(message.username),
-            ),
-          ),
-          SizedBox(width: 5),
-          if (message.isAdmin)
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 4, vertical: 2),
-              color: Colors.redAccent,
-              child: Text('Admin',
-                  style: TextStyle(color: Colors.white, fontSize: 10)),
-            )
-        ],
-      ),
-      subtitle: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(message.text),
-          SizedBox(height: 4),
-          Text(
-            message.status == MessageStatus.sending
-                ? 'Sending...'
-                : 'Sent at ${_formatTime(message.timestamp)}',
-            style: TextStyle(fontSize: 10, color: Colors.grey),
-          )
-        ],
-      ),
-      onLongPress: () => _showMessageActions(message),
-    );
-  }
-
-  void _showMessageActions(Message message) {
-    // Action buttons: copy, edit, delete (with confirm)
-    showModalBottomSheet(
-      context: context,
-      builder: (ctx) {
-        return Wrap(
-          children: [
-            ListTile(
-              leading: Icon(Icons.copy),
-              title: Text('Copy'),
-              onTap: () {
-                // TODO: Copy message to clipboard
-                Navigator.pop(ctx);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.edit),
-              title: Text('Edit'),
-              onTap: () {
-                // TODO: Populate text field for editing
-                Navigator.pop(ctx);
-              },
-            ),
-            ListTile(
-              leading: Icon(Icons.delete),
-              title: Text('Delete'),
-              onTap: () {
-                // TODO: Confirm and delete message
-                Navigator.pop(ctx);
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Color _getRandomColor(String username) {
-    final colors = [Colors.blue, Colors.green, Colors.orange, Colors.purple];
-    return colors[username.hashCode % colors.length];
-  }
-
-  String _formatTime(DateTime time) {
-    return "${time.hour.toString().padLeft(2, '0')}:${time.minute.toString().padLeft(2, '0')}";
-  }
+  final String chatName = "Chat room";
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading:
-            IconButton(icon: Icon(Icons.arrow_back), onPressed: () {/* Back */}),
-        title: Text('Branco'),
-        actions: [
-          IconButton(icon: Icon(Icons.search), onPressed: () {/* Search */}),
-        ],
+        title: Text(chatName),
       ),
-      body: Column(
+      body: ListView(
+        padding: EdgeInsets.all(10.0),
         children: [
-          // Unread messages title (placeholder)
-          Container(
-            padding: EdgeInsets.all(8),
-            alignment: Alignment.center,
-            child: Text('${messages.length} unread messages'),
-          ),
-          Expanded(
-            child: ListView.builder(
-              controller: _scrollController,
-              itemCount: messages.length,
-              itemBuilder: (context, index) =>
-                  _buildMessageBubble(messages[index]),
-            ),
-          ),
-          Divider(height: 1),
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 8),
-            color: Colors.white,
-            child: Row(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.attach_file),
-                  onPressed: () {
-                    // TODO: Attach file or image
-                  },
-                ),
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      hintText: 'Type a message',
-                      border: InputBorder.none,
-                    ),
-                    onTap: () {
-                      // Re-enable auto-scroll when interacting
-                      autoScroll = true;
-                    },
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.send),
-                  onPressed: _sendMessage,
-                ),
-              ],
-            ),
-          )
+          _buildMessage("Ciao!", true),
+          _buildMessage("Ciao! Come stai?", false),
+          _buildMessage("Tutto bene, grazie!", true),
         ],
       ),
     );
   }
-}
 
-enum MessageStatus { sending, sent }
-
-class Message {
-  final String username;
-  final String text;
-  final bool isAdmin;
-  final DateTime timestamp;
-  final MessageStatus status;
-
-  Message({
-    required this.username,
-    required this.text,
-    required this.isAdmin,
-    required this.timestamp,
-    required this.status,
-  });
+  Widget _buildMessage(String text, bool isMe) {
+    return Align(
+      alignment: isMe ? Alignment.centerRight : Alignment.centerLeft,
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 5.0),
+        padding: EdgeInsets.all(10.0),
+        decoration: BoxDecoration(
+          color: isMe ? Colors.blue[200] : Colors.grey[300],
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Text(text),
+      ),
+    );
+  }
 }
